e9e01f705215e6dbdf31f5b4c67c509fdca470fe
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-10-10 21:43:27 +0530

    An IF ELSE program using IS operator.

---
+++ ifelse1.py
@@ -0,0 +1,6 @@
+x = ("apple","ball","cat")
+y = ("ball","cat","apple")
+if x is y:
+    print("World is a beautiful place to live in.")
+else:
+    print("Opportunities are like sunrise, if one waits for too long, miss them.")
